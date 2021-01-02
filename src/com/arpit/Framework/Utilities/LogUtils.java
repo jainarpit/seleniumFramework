ec44189770209587315f40438ff3d122232ab773
Arpit Jain <arpitjain2402@gmail.com>
2021-01-03 00:41:57 +0530

    add support for capturing log using custom logs

--- src/com/arpit/Framework/Utilities/LogUtils.java
+++ src/com/arpit/Framework/Utilities/LogUtils.java
@@ -1,4 +1,49 @@
-package Framework.Utilities;
+package Utilities;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class LogUtils {
+
+    /*
+    To Have:
+    1. File Format for the log name
+    2. Create Log File
+    3. Write Message within the log
+     */
+    ZonedDateTime zonedDateTime = ZonedDateTime.now();
+    DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("ddMMyyyyHHMMSS");
+    String fileNameFormat = zonedDateTime.format(dateTimeFormatter);
+    private BufferedWriter bufferedWriter = null;
+
+    public void createLogFile() {
+        String path = "./LogFolder/";
+        try {
+            File dir = new File(path);
+            if (!dir.exists())
+                dir.mkdir();
+
+            File logFile = new File(path + "/" + fileNameFormat + ".log");
+
+            FileWriter fileWriter = new FileWriter(logFile.getAbsoluteFile());
+            bufferedWriter = new BufferedWriter(fileWriter);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeLog(String message) {
+        try {
+            bufferedWriter.write(message);
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/test/arpit/tests/LoginTest.java
+++ src/test/arpit/tests/LoginTest.java
@@ -1,21 +1,31 @@
 package arpit.tests;
 
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-import base.BrowserType;
-import base.DriverContext;
-import base.FrameworkInitialize;
+import Utilities.ExcelUtil;
+import Utilities.LogUtils;
 import arpit.Pages.EmployeeListPage;
 import arpit.Pages.HomePage;
 import arpit.Pages.LoginPage;
+import base.BrowserType;
+import base.DriverContext;
+import base.FrameworkInitialize;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class LoginTest extends FrameworkInitialize {
 
     @BeforeTest
     public void setup() {
+        LogUtils log = new LogUtils();
+        log.createLogFile();
+        log.writeLog("Framework Initialized");
         initializeBrowser(BrowserType.CHROME);
         DriverContext.Browser.GoToURL("http://eaapp.somee.com/");
+        try {
+            ExcelUtil util = new ExcelUtil("/Users/arpit/Documents/SelfLearning/SeleniumFrameworkSetup/seleniumFramework/data.xls");
+        } catch (Exception e) {
+
+        }
     }
 
     @Test
